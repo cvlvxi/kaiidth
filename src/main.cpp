e6b75c3dc77733f5723df339100189272e1d8f7c
cvlvxi <cvlvxi@outlook.com>
2021-03-29 11:11:47 +1100

    Actually display a window

--- src/main.cpp
+++ src/main.cpp
@@ -7,5 +7,14 @@
 int main() {
     glfwInit();
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Hello World", nullptr, nullptr);
+
+    while(!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
     return 0;
 }
