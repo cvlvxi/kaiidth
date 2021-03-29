34641233ef803f6f707cb8de7e45a0e560ff44a5
cvlvxi <cvlvxi@outlook.com>
2021-03-29 11:17:37 +1100

    Working with glm

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #define GLFW_INCLUDE_VULKAN
 
 #include <iostream>
+#include <glm/vec4.hpp>
 #include <GLFW/glfw3.h>
 
 
@@ -8,6 +9,8 @@ int main() {
     glfwInit();
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     GLFWwindow* window = glfwCreateWindow(800, 600, "Hello World", nullptr, nullptr);
+
+    glm::vec4 vec;
 
     while(!glfwWindowShouldClose(window)) {
         glfwPollEvents();
